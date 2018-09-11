4e1a853a82827943d59733e25d755139e46a599f
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-09-11 16:54:14 +0100

    error handling

--- gaw dope.py
+++ gaw dope.py
@@ -4,7 +4,7 @@ import time
 import math
 import os
 import geopy.distance
-target = "Terminus"  #Enter Name Here
+target = "Nemosupremo"  #Enter Name Here
 def calculate_initial_compass_bearing(pointA, pointB):
     lat1 = math.radians(pointA[0])
     lat2 = math.radians(pointB[0])
@@ -16,50 +16,52 @@ def calculate_initial_compass_bearing(pointA, pointB):
     initial_bearing = math.degrees(initial_bearing)
     compass_bearing = (initial_bearing + 360) % 360
     return compass_bearing
+try:
+	while True:
+		with urllib.request.urlopen("http://dcs.hoggitworld.com/") as url:
+			data = json.loads(url.read().decode())
+			for i in range(len(data["objects"])):
+				if data["objects"][i]["Flags"]["Human"] == True:
+					if data["objects"][i]["UnitName"] == target:
+						MyLat = data["objects"][i]["LatLongAlt"]["Lat"]
+						MyLon = data["objects"][i]["LatLongAlt"]["Long"]
+						MyAlt = data["objects"][i]["LatLongAlt"]["Alt"]
 
-while True:
-    with urllib.request.urlopen("http://dcs.hoggitworld.com/") as url:
-        data = json.loads(url.read().decode())
-        for i in range(len(data["objects"])):
-            if data["objects"][i]["Flags"]["Human"] == True:
-                if data["objects"][i]["UnitName"] == target:
-                    MyLat = data["objects"][i]["LatLongAlt"]["Lat"]
-                    MyLon = data["objects"][i]["LatLongAlt"]["Long"]
-                    MyAlt = data["objects"][i]["LatLongAlt"]["Alt"]
+			os.system('cls' if os.name=='nt' else 'clear')
+			count = 0
+			print("---------------------------")
+			print("Bogey dope for "+target+".")
+			print("---------------------------\n")
+			for i in range(len(data["objects"])):
+				if data["objects"][i]["Flags"]["Human"] == False:
+					if data["objects"][i]["Coalition"] == "Allies" and data["objects"][i]["Flags"]["Born"] == True:
+						Type = False
+						if data["objects"][i]["Name"] == "Su-27":
+							Type = "Su-27"
+						elif data["objects"][i]["Name"] == "F-5E-3":
+							Type = "F5"
+						elif data["objects"][i]["Name"] == "Su-25T":
+							Type = "Su-25T"
+						elif data["objects"][i]["Name"] == "Mi-26":
+							Type = "Mi-26"
 
-        os.system('cls' if os.name=='nt' else 'clear')
-        count = 0
-        print("---------------------------")
-        print("Bogey dope for "+target+".")
-        print("---------------------------\n")
-        for i in range(len(data["objects"])):
-            if data["objects"][i]["Flags"]["Human"] == False:
-                if data["objects"][i]["Coalition"] == "Allies" and data["objects"][i]["Flags"]["Born"] == True:
-                    Type = False
-                    if data["objects"][i]["Name"] == "Su-27":
-                        Type = "Su-27"
-                    elif data["objects"][i]["Name"] == "F-5E-3":
-                        Type = "F5"
-                    elif data["objects"][i]["Name"] == "Su-25T":
-                        Type = "Su-25T"
-                    elif data["objects"][i]["Name"] == "Mi-26":
-                        Type = "Mi-26"
-
-                    Lat = data["objects"][i]["LatLongAlt"]["Lat"]
-                    Lon = data["objects"][i]["LatLongAlt"]["Long"]
-                    Alt = data["objects"][i]["LatLongAlt"]["Alt"]
-                    if Type != False:
-                        MyPos = (MyLat, MyLon)
-                        TPos = (Lat, Lon)
-                        Distance = geopy.distance.distance(MyPos,TPos).nm
-                        A = (MyLat, MyLon)
-                        B = (Lat, Lon)
-                        Bearing = calculate_initial_compass_bearing(A, B)
-                        print("Target Type:\t"+Type)
-                        print("Distance:\t"+str(round(Distance,1))+" Miles")
-                        print("Bearing:\t"+str(round(Bearing,0)))
-                        print("Altitude:\tAngels "+str(round(Alt/1000,1)))
-                        print("\r")
-                        count = count + 1
-    print("Target Count:\t"+str(count))
-    time.sleep(10)
+						Lat = data["objects"][i]["LatLongAlt"]["Lat"]
+						Lon = data["objects"][i]["LatLongAlt"]["Long"]
+						Alt = data["objects"][i]["LatLongAlt"]["Alt"]
+						if Type != False:
+							MyPos = (MyLat, MyLon)
+							TPos = (Lat, Lon)
+							Distance = geopy.distance.distance(MyPos,TPos).nm
+							A = (MyLat, MyLon)
+							B = (Lat, Lon)
+							Bearing = calculate_initial_compass_bearing(A, B)
+							print("Target Type:\t"+Type)
+							print("Distance:\t"+str(round(Distance,1))+" Miles")
+							print("Bearing:\t"+str(round(Bearing,0)))
+							print("Altitude:\tAngels "+str(round(Alt/1000,1)))
+							print("\r")
+							count = count + 1
+		print("Target Count:\t"+str(count))
+		time.sleep(10)
+except:
+	print("Something went wrong (User probably not found)\n")
