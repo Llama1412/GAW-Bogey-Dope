222cca2e633be1ec0bdbb05bb45d2681d4d65b81
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-09-12 19:34:17 +0100

    moved the defs to outside of the while loop

--- BogeyDope.py
+++ BogeyDope.py
@@ -37,6 +37,40 @@ threats = {
 	"MiG-29S": THREAT.HIGH,
 	"MiG-31": THREAT.HIGH
 }
+def calculate_initial_compass_bearing(pointA, pointB):
+	lat1 = math.radians(pointA[0])
+	lat2 = math.radians(pointB[0])
+	diffLong = math.radians(pointB[1] - pointA[1])
+	x = math.sin(diffLong) * math.cos(lat2)
+	y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+	* math.cos(lat2) * math.cos(diffLong))
+	initial_bearing = math.atan2(x, y)
+	initial_bearing = math.degrees(initial_bearing)
+	compass_bearing = (initial_bearing + 360) % 360
+	return compass_bearing
+
+	def make_bogey(target, MyPos):
+		Lat = target["LatLongAlt"]["Lat"]
+		Lon = target["LatLongAlt"]["Long"]
+		Alt = target["LatLongAlt"]["Alt"]
+		TPos = (Lat, Lon)
+		Distance = geopy.distance.distance(MyPos,TPos).nm
+		A = (MyLat, MyLon)
+		B = (Lat, Lon)
+		Bearing = calculate_initial_compass_bearing(A, B)
+		return Bogey(Type, Bearing, Distance, round(Alt/1000,1), Threat)
+
+		def print_bogeydope(player_name, bogeys):
+			print(colored("---------------------------","cyan"))
+			print(colored("Bogey dope for "+player_name+".","cyan"))
+			print(colored("---------------------------\n","cyan"))
+			for bogey in bogeys:
+				print(colored("Target Type:\t"+bogey.Type,bogey.Threat))
+				print(colored("Distance:\t"+str(round(bogey.Range,1))+" Miles",bogey.Threat))
+				print(colored("Bearing:\t"+str(round(bogey.Bearing,0)),bogey.Threat))
+				print(colored("Altitude:\tAngels "+str(round(bogey.Altitude/1000,1)),bogey.Threat))
+				print("\r")
+				print("Target Count:\t"+str(len(bogeys)))
 while True:
 	os.system('cls' if os.name=='nt' else 'clear')
 	print("Welcome to the GAW GCI Tool.")
@@ -47,42 +81,6 @@ while True:
 	if choice == str("1"):
 		try:
 			target = input("Enter user:\t")  #Enter Name Here
-			#target = "Skillhouse246"
-			def calculate_initial_compass_bearing(pointA, pointB):
-			    lat1 = math.radians(pointA[0])
-			    lat2 = math.radians(pointB[0])
-			    diffLong = math.radians(pointB[1] - pointA[1])
-			    x = math.sin(diffLong) * math.cos(lat2)
-			    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
-			            * math.cos(lat2) * math.cos(diffLong))
-			    initial_bearing = math.atan2(x, y)
-			    initial_bearing = math.degrees(initial_bearing)
-			    compass_bearing = (initial_bearing + 360) % 360
-			    return compass_bearing
-
-			def make_bogey(target, MyPos):
-				Lat = target["LatLongAlt"]["Lat"]
-				Lon = target["LatLongAlt"]["Long"]
-				Alt = target["LatLongAlt"]["Alt"]
-				TPos = (Lat, Lon)
-				Distance = geopy.distance.distance(MyPos,TPos).nm
-				A = (MyLat, MyLon)
-				B = (Lat, Lon)
-				Bearing = calculate_initial_compass_bearing(A, B)
-				return Bogey(Type, Bearing, Distance, round(Alt/1000,1), Threat)
-
-			def print_bogeydope(player_name, bogeys):
-				print(colored("---------------------------","cyan"))
-				print(colored("Bogey dope for "+player_name+".","cyan"))
-				print(colored("---------------------------\n","cyan"))
-				for bogey in bogeys:
-						print(colored("Target Type:\t"+bogey.Type,bogey.Threat))
-						print(colored("Distance:\t"+str(round(bogey.Range,1))+" Miles",bogey.Threat))
-						print(colored("Bearing:\t"+str(round(bogey.Bearing,0)),bogey.Threat))
-						print(colored("Altitude:\tAngels "+str(round(bogey.Altitude/1000,1)),bogey.Threat))
-						print("\r")
-				print("Target Count:\t"+str(len(bogeys)))
-
 			while True:
 					with urllib.request.urlopen("http://state.hoggitworld.com/") as url:
 						data = json.loads(url.read().decode())
